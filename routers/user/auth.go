f8f389b3e3d0bb5d8516b82eaa8e3ffe95259fc1
Wim <wim@42.be>
2020-06-18 21:11:39 +0200

    Redirect to login page on signout

--- routers/user/auth.go
+++ routers/user/auth.go
@@ -1064,7 +1064,7 @@ func SignOut(ctx *context.Context) {
 		})
 	}
 	HandleSignOut(ctx)
-	ctx.Redirect(setting.AppSubURL + "/")
+	ctx.Redirect(setting.AppSubURL + "/user/login")
 }
 
 // SignUp render the register page
